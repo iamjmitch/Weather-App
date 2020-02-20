fd75d31d924094e67fb31500eb0121d723f4d9b2
James Mitchell <iamjmitch1990@gmail.com>
2020-02-20 21:11:10 +1000

    added if statements to load

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    error => console.log(error)
-  );
-  return <div>Latitude: </div>;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, errorMessage: "" };
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      error => {
+        this.setState({ errorMessage: error.message });
+      }
+    );
+  }
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage} </div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat} </div>;
+    }
+    return <div>Loading....</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
